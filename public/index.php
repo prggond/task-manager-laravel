0cd5b1982e384eed1f7f5acccc35edc086348a49
Praveen gond <p50153168@gmail.com>
2026-03-18 15:14:32 +0530

    CORS in index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+
+
+// ✅ CORS headers seedha PHP mein
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
