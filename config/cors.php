2d84cfafb68cec7181192025b4eeecbb8caaa265
Praveen gond <p50153168@gmail.com>
2026-03-18 14:18:45 +0530

    CORS fix for Vercel

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,15 @@
 <?php
 
 return [
-
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'], // 🔥 temp fix
-
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://task-manager-react-vn4u.vercel.app',  // ✅ Vercel URL add karo
+    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
